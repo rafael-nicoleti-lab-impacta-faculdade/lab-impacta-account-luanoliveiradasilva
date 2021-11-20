13ab65a0c1c11f54a6cd282752bff8668df44299
Luan Nascimento <luanoliveira1107@gmail.com>
2021-11-20 09:54:11 -0300

    Add handler.

---
+++ investment/src/main/java/br/com/lab/impacta/investment/handler/ControllerExceptionHandler.java
@@ -0,0 +1,4 @@
+package br.com.lab.impacta.investment.handler;
+
+public class ControllerExceptionHandler {
+}
---
+++ investment/src/main/java/br/com/lab/impacta/investment/handler/exception/InvestimentProductDontExistException.java
@@ -0,0 +1,4 @@
+package br.com.lab.impacta.investment.handler.exception;
+
+public class InvestimentProductDontExistException {
+}
